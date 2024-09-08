e7912107a654eb189c786c4feea4993e6bf7abbe
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-08 18:23:15 +0000

    Atualizando a ordem das mensagens da tela inicial

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
     return <div>
         <h1>Ristau, eu te amo!</h1>
-        <p>Obrigado pela compania enquanto estudo! Prometo me esforçar ao máximo.</p>
         <h2>Você é linda DEMAIS!!!</h2>
+        <p>Obrigado pela compania enquanto estudo! Prometo me esforçar ao máximo.</p>
     </div>
 }
 
