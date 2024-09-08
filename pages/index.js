e7ddc0d4df5b46b56804f5db1b71deaa6b0a3367
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-08 17:54:24 +0000

    Adicionando Parágrafo e Subtítulo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Ristau, eu te amo!</h1>
+    return <div>
+        <h1>Ristau, eu te amo!</h1>
+        <p>Obrigado pela compania enquanto estudo! Prometo me esforçar ao máximo.</p>
+        <h2>Você é linda DEMAIS!!!</h2>
+    </div>
 }
 
 export default Home;
