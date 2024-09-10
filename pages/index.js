5f5ac5a88d70a8d30c40434dee5890ca1cadb0d8
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-10 12:10:17 +0000

    Adiciona a extensã que lida com o `.editorconfig` e corrige a identação do `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Mensagem from "../components/component";
 import styles from "../styles.module.css";
 
 function Home() {
-    return <div className={styles.wide}>
-        <div className={styles.text}>
-            <Mensagem/>
-        </div>
+  return <div className={styles.wide}>
+    <div className={styles.text}>
+      <Mensagem/>
     </div>
-}
+  </div>
+}  
 
 export default Home;
