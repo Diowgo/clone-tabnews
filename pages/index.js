fbfe6317bcb57bc3988cd2cd781d86653b0f4780
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-13 23:34:46 +0000

    Adicionando estilos e modificando a página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import Mensagem from "../components/component";
 import styles from "../styles.module.css";
 
 function Home() {
   return (
-    <div className={styles.wide}>
-      <div className={styles.text}>
-        <Mensagem />
-      </div>
-    </div>
+    <main className={styles.centro}>
+      <h1>O que é esse site?</h1>
+      <p>
+        Oi! Eu sou o Diogo. Adoro fazer olimpíadas científicas sem muito
+        compromisso e estudar programação! Recentemente comecei o curso.dev, e
+        estou adorando essa jornada de estudos! Esse site é tipo o meu TCC.
+        Quero que esse site se torne um registro da minha história como
+        programador: Um lugar em que eu documento tudo aquilo por que passei, o
+        que aprendi e por onde estudei. Super me imagino, no futuro, olhando pra
+        esse site todo feliz por ter documentado todo o processo!
+      </p>
+      <h2>Simplesmente é um upload do meu cérebro</h2>
+      <p>E estou ansioso para desenvolver esse projeto com muito carinho.</p>
+    </main>
   );
-}
-
-function Camisa() {
-  console.log("Barcelona FC");
 }
 
 export default Home;
