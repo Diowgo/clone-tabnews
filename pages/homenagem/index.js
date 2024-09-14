ff200c90e9c3a8cb310aee0d71a07fa26991bd30
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-14 00:32:10 +0000

    Adicionando página de homenagem

---
+++ pages/homenagem/index.js
@@ -0,0 +1,7 @@
+import styles from "../../styles.module.css";
+
+export default function Homenagem() {
+  return (
+    <h1 className={styles.fonte}>À minha namorada Ristau, eu te amo! ❤️</h1>
+  );
+}
