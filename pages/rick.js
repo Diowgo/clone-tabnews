93bfa1c01382d48aee88fd4e2b40cac2b6c40439
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-14 19:22:13 +0000

    Adicionando mensagem pro Rick

---
+++ pages/rick.js
@@ -0,0 +1,9 @@
+import styles from "../styles.module.css";
+
+export default function Mensagem() {
+  return (
+    <h1 className={styles.centro}>
+      Vai ficar tudo bem, meu caro! Boa sorte nos seus estudos ❤️
+    </h1>
+  );
+}
