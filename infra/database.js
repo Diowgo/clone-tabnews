51111ecbaca635de0540e1dea929a43f27d6ef0c
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-24 00:28:31 +0000

    +  e adição de variáveis de ambiente

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+  });
+  await client.connect();
+  const res = await client.query(queryObject);
+  await client.end();
+  return res;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
+import database from "../../../../infra/database.js";
+
 async function status(request, response) {
+  const res = await database.query("SELECT 1 + 1 as UM_MAIS_UM;");
+  console.log(res);
   response.status(200).json({ "Tá tudo ok, a resposta é": 200 });
 }
 
