ec46ac47c0594367e57a6c6cc6d23a46c8f1d68e
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-09-24 00:47:31 +0000

    fixing wrong .env references

--- infra/database.js
+++ infra/database.js
@@ -6,7 +6,7 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
   });
   await client.connect();
   const res = await client.query(queryObject);
