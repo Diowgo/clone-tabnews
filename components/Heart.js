a3591dd77da5ab5c992588443c4aa56a5fc63ec5
Diowgo <126202430+Diowgo@users.noreply.github.com>
2024-10-05 19:35:34 +0000

    Página do EPIC

--- components/Heart.js
+++
@@ -1,3 +0,0 @@
-export default function Heart() {
-  return <h1 style={{}}>❤️</h1>;
-}
---
+++ pages/homenagem/epic.js
@@ -0,0 +1,10 @@
+import styles from "styles.module.css";
+
+export default function Epic() {
+  return (
+    <div class={`${styles.centro} ${styles.fonte}`}>
+      <h1>Página dedicada aos fãs de EPIC</h1>
+      <p>And hunger is so heavy (hunger is so heavy)</p>
+    </div>
+  );
+}
